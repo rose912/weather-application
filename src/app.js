f14e6d22a93e27fab5b2a4906bf0001de11d2791
rose912 <chioma.osuagwu@ymail.com>
2023-10-19 15:39:51 -0400

    Remove console

--- src/app.js
+++ src/app.js
@@ -63,7 +63,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
